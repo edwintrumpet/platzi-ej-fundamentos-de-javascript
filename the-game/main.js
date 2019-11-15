8e99b1b5bef4f6f0897939f2cd96b5ea081ceb6b
Edwin García <spark.com.co@gmail.com>
2019-11-15 15:36:20 -0500

    Generando una secuencia de números

--- the-game/main.js
+++ the-game/main.js
@@ -7,10 +7,22 @@ const verde = document.getElementById('verde')
 class Juego {
     constructor() {
         this.inicializar()
+        this.generarSecuencia()
     }
 
     inicializar() {
         btnEmpezar.classList.add('hide')
+        this.nivel = 1
+        this.colores = {
+            celeste,
+            violeta,
+            naranja,
+            verde
+        }
+    }
+
+    generarSecuencia() {
+        this.secuencia = new Array(10).fill(0).map(n => Math.floor(Math.random() * 4))
     }
 }
 
