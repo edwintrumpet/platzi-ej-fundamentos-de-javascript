18560dc6af71ab2036a563e74ece261f7596686d
Edwin García <spark.com.co@gmail.com>
2019-11-15 12:15:49 -0500

    Herencia prototipal

--- learning/classes/prototipe.js
+++ learning/classes/prototipe.js
@@ -1,3 +1,10 @@
+function heredaDe(prototipoHijo, prototipoPadre) {
+    let fn = function() {}
+    fn.prototype = prototipoPadre.prototype
+    prototipoHijo.prototype = new fn
+    prototipoHijo.prototype.constructor = prototipoHijo
+}
+
 function Persona(nombre, apellido, altura) {
     this.nombre  = nombre
     this.apellido = apellido
@@ -8,8 +15,25 @@ Persona.prototype.saludar = function() {
     console.log(`Hola, me llamo ${this.nombre} ${this.apellido}`)
 }
 
-const edwin = new Persona('Edwin', 'García', 1.70)
+Persona.prototype.despedida = function() {
+    console.log(`bye`)
+}
+
+function Desarrollador(nombre, apellido) {
+    this.nombre  = nombre
+    this.apellido = apellido
+}
+
+heredaDe(Desarrollador, Persona)
+
+Desarrollador.prototype.saludar = function() {
+    console.log(`Hola, me llamo ${this.nombre} ${this.apellido} y soy desarrollador`)
+}
+
+const edwin = new Desarrollador('Edwin', 'García', 1.70)
 const lorena = new Persona('Lorena', 'Muñoz', 1.50)
 
 edwin.saludar()
 lorena.saludar()
+edwin.despedida()
+lorena.despedida()
